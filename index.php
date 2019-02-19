b23b9caa72d30e80fec941096ab641848f243aa0
B Dub <bdub@Bs-iMac.local>
2019-02-18 19:51:29 -0700

    init2

--- index.php
+++ index.php
@@ -1,9 +1,47 @@
-<!DOCTYPE>
-<html>
+<html lang="en">
     <head>
-        <title>PhP for life</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>phpTest</title>
     </head>
     <body>
-        <p><?php echo date("F jS, Y"); ?></p>
+        
+        <?php 
+            echo "Hi There! "; //faster loaded
+            print 10+5;
+        ?>
+        <form method="GET" action="">
+            <input name="person" type="text" >
+            <button>SUBMIT</button>
+        </form>
+        <!-- variables -->
+        <?php 
+            $name = $_GET['person'];
+            echo $name." is a handsome fellow!";
+        ?>
+        <!-- functions -->
+        <?php 
+            echo "String Length of handsome is: ";
+            echo strlen("handsome")
+
+        ?>
+        <!-- dataTypes -->
+        <?php 
+            //String
+            //Integer
+                // 20;
+            //Float
+                // 20.5;
+            //Boolean
+                // true = 1;
+                // false = 0;
+            //Array
+            $names = array("Daniel", "Dennis", "Michael");
+            echo $names['0'];
+
+            //= equals to == is the same value === is the same datatype as well.
+
+        ?>
     </body>
 </html>
